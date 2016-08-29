b0e90bf24c8d4e3d2c87404c5b530f2b4f44de57
Daniel Fernandes <daniel.fernandes@namics.com>
2016-08-29 16:01:12 +0200

    added to infotable commit

--- components/modules/detailview/js/detailview.js
+++ components/modules/detailview/js/detailview.js
@@ -12,9 +12,6 @@
 
 		init: function ($ctx, sandbox, modId) {
 			this._super($ctx, sandbox, modId);
-			// var mod = this;
-			// var player = mod.$ctx.data('player');
-			// $('body').addClass(player);
 
 		},
 
@@ -22,16 +19,100 @@
 			var mod = this,
 				$ctx = mod.$ctx;
 
+			this.getPlayerInfo();
 
 			callback();
 		},
 
-		after: function () {
-			var mod = this,
-				$ctx = mod.$ctx;
+		getPlayerInfo: function () {
+		var mod = this;
+		$.ajax({
+			url: "/views/data",
+			method: "post",
+			success: function (response) {
+				mod.processInfos(response);
+			}
+		})
+	},
+	processInfos: function (data) {
+		var mod = this;
+		var player = mod.$ctx.data('player');
+		var season = mod.$ctx.data('season');
+		$('body').addClass(player);
+
+		$(data[player].info
+		).each(
+			function (index, item) {
+				var html = "<tr>";
+				html += "<td>";
+				html += "Name:";
+				html += "</td>";
+				html += "<td>";
+				html += item.firstLastName;
+				html += "</td>";
+				html += "<td>";
+				html += "Geburtsdatum:";
+				html += "</td>";
+				html += "<td>";
+				html += item.gebDatum;
+				html += "</td>";
+				html += "</tr>";
+				html += "<tr>";
+				html += "<td>";
+				html += "Nationalität:";
+				html += "</td>";
+				html += "<td>";
+				html += item.nationalitaet;
+				html += "</td>";
+				html += "<td>";
+				html += "Geburtsort:";
+				html += "</td>";
+				html += "<td>";
+				html += item.geburtsort;
+				html += "</td>";
+				html += "</tr>";
+				html += "<tr>";
+				html += "<td>";
+				html += "Position:";
+				html += "</td>";
+				html += "<td>";
+				html += item.positionFeld;
+				html += "</td>";
+				html += "<td>";
+				html += "Grösse:";
+				html += "</td>";
+				html += "<td>";
+				html += item.groesse;
+				html += "</td>";
+				html += "</tr>";
+				html += "<tr>";
+				html += "<td>";
+				html += "Berater:";
+				html += "</td>";
+				html += "<td>";
+				html += item.berater;
+				html += "</td>";
+				html += "<td>";
+				html += "Länderspiele/Tore:";
+				html += "</td>";
+				html += "<td>";
+				html += item.landSpieleSlashTore;
+				html += "</td>";
+				html += "</tr>";
+
+				console.log(html);
+				$(".js-flat-table").append(html);
+			}
+		)
+	},
+
+	after: function () {
+		var mod = this,
+			$ctx = mod.$ctx;
 
 
-		}
+	}
 
-	});
+})
+	;
 }(Tc.$));
