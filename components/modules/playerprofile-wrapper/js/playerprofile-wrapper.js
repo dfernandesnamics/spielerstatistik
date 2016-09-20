db6553d441f5fdc5bc4e5ab85e1e21d4d79dae83
Daniel Fernandes <daniel.fernandes@namics.com>
2016-09-20 10:15:04 +0200

    get data from json and handlebars methods to fill data

--- components/modules/playerprofile-wrapper/js/playerprofile-wrapper.js
+++ components/modules/playerprofile-wrapper/js/playerprofile-wrapper.js
@@ -1,4 +1,4 @@
-(function($) {
+(function ($) {
 	"use strict";
 	/**
 	 * playerprofile-wrapper module implementation.
@@ -10,43 +10,86 @@
 	 */
 	Tc.Module.PlayerprofileWrapper = Tc.Module.extend({
 
-		init: function($ctx, sandbox, modId) {
+		init: function ($ctx, sandbox, modId) {
 			this._super($ctx, sandbox, modId);
 
 
-
 		},
 
-		on: function(callback) {
+		on: function (callback) {
 			var mod = this,
 				$ctx = mod.$ctx;
 
-		jsonData();
-
+			this.jsonData();
 			callback();
 		},
-		jsonData: function (evt, data) {
-					var mod = this;
-					$.ajax({
-						url: "/views/data",
-						method: "post ",
-						success: function (response) {
-							var player = mod.$ctx.data('player');
-							var season = mod.$ctx.data('season');
-							if (typeof (response[player].seasons[season]) === 'undefined') {
-								alert('not defined');
-							}
-							mod.processData(response);
-							console.log(data);
-		
-						},
-						error: function (error) {
-							alert(error);
-						}
-					})
-				},
 
-		after: function() {
+
+		jsonData: function (evt, data) {
+			var mod = this;
+			$.ajax({
+				url: "/views/data",
+				method: "post",
+				success: function (response) {
+					var player = mod.$ctx.data('player');
+					mod.processJson(response);
+
+				}
+			});
+
+		},
+		processJson: function (data) {
+			var mod = this;
+			var player = $('.js-player-id', mod.$ctx).val();
+
+			console.log(data[player]);
+			mod.buildTemplatePicProfile({
+				profilPic: data[player].infos.profilePicSrc,
+				name: data[player].infos.firstLastName,
+				playerName: data[player].infos.cssClass
+			});
+			mod.buildTemplateInfoTable({
+				playerName: data[player]
+			});
+			mod.buildTemplateSeasonTable({
+				playerName: data[player],
+				playerId: data[player]
+			})
+			
+
+		},
+		
+		buildTemplatePicProfile: function (dataObject) {
+			var mod = this;
+			var templateSrc = $('#tpl-profpic').html(),
+				template = Handlebars.compile(templateSrc),
+				html = template(dataObject),
+				$outlet = $('.js-outlet-profilpic-module', mod.$ctx);
+			
+				$outlet.html(html);
+			
+		},
+		buildTemplateInfoTable: function(dataObject) {
+			var mod = this;
+			var templateSrc = $('#tpl-inftbl').html(),
+				template = Handlebars.compile(templateSrc),
+				html = template(dataObject),
+				$outlet = $('.js-outlet-info-season-table', mod.$ctx);
+			
+				$outlet.html(html);
+		},
+		buildTemplateSeasonTable: function(dataObject) {
+			var mod = this;
+			var templateSrc = $('#tpl-seasntbl').html(),
+				template = Handlebars.compile(templateSrc),
+				html = template(dataObject),
+				$outlet = $('.js-outlet-info-season-table', mod.$ctx);
+			
+				$outlet.html(html);
+		},
+		
+		
+		after: function () {
 			var mod = this,
 				$ctx = mod.$ctx;
 
